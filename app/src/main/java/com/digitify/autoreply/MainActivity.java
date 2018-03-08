7652c2c19a15c9ab7f97e688abffe8275f6a1a06
Sufyan Shabbir <sufyan@digitify.com>
2018-03-08 10:09:06 +0500

    - basic setup of main activity

--- app/src/main/java/com/digitify/autoreply/MainActivity.java
+++ app/src/main/java/com/digitify/autoreply/MainActivity.java
@@ -2,6 +2,7 @@ package com.digitify.autoreply;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        initObj();
+        initListeners();
+
     }
+
+    private void initViews() {
+    }
+
+    private void initObj() {
+    }
+
+    private void initListeners() {
+    }
+
+    final View.OnClickListener mGlobal_OnClickListener = new View.OnClickListener() {
+        public void onClick(final View v) {
+            switch (v.getId()) {
+              /*  case R.id.btnLogin: {
+
+                    break;
+                }*/
+            }
+        }
+
+    };
+
 }
